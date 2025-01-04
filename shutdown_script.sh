79b0a4f1f9e6c0cb9a087de1ec00dce0f66ca032
designworld <47788179+designworld-ca@users.noreply.github.com>
2025-01-04 14:12:38 -0500

    Update shutdown_script.sh

--- shutdown_script.sh
+++ shutdown_script.sh
@@ -2,7 +2,8 @@
 #!/bin/sh
 # cat shutdown_script.sh
 # shut down the docker containers in vm 100.  A fast shutdown can cause file corruption
-echo "UPS charge is half used for $(hostname)." | mail -s "Power Shutdown" <your email here>
+now = "$(date)"
+echo "UPS charge is half used for $(hostname) on $now." | mail -s "Power Shutdown" <your email here>
 qm guest exec 100 -- /bin/bash -c "home/<redacted>/safe_shutdown.sh"
 shutdown -P +0
 
