a6bb9097c43c580be0d67adedf5baca72d8867ba
designworld <47788179+designworld-ca@users.noreply.github.com>
2025-01-04 09:24:02 -0500

    Update shutdown_script.sh

--- shutdown_script.sh
+++ shutdown_script.sh
@@ -1,7 +1,7 @@
-# don'r forget to make the script executable with chmod +x <your scriptname>
+# don't forget to make the script executable with chmod +x <your scriptname>
 #!/bin/sh
 # cat shutdown_script.sh
-# shut down the docker containers in vm 100.  A forced shutdown can cause file corruption
+# shut down the docker containers in vm 100.  A fast shutdown can cause file corruption
 echo "UPS charge is half used for $(hostname)." | mail -s "Power Shutdown" <your email here>
 qm guest exec 100 -- /bin/bash -c "home/<redacted>/safe_shutdown.sh"
 shutdown -P +0
